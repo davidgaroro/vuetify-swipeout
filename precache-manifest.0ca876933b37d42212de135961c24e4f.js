02962ca88979b488a03f7134db85a91eb7afbbca
davidgaroro <davidgaroro@gmail.com>
2021-11-05 03:13:11 +0100

    Add files to production

---
+++ precache-manifest.0ca876933b37d42212de135961c24e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a160b7ff396757237df",
+    "url": "/vuetify-swipeout/css/app.c5c9b156.css"
+  },
+  {
+    "revision": "8a160b7ff396757237df",
+    "url": "/vuetify-swipeout/js/app.b5d47caf.js"
+  },
+  {
+    "revision": "d5e71dc8fe425eadddde",
+    "url": "/vuetify-swipeout/css/chunk-vendors.25308925.css"
+  },
+  {
+    "revision": "d5e71dc8fe425eadddde",
+    "url": "/vuetify-swipeout/js/chunk-vendors.a7c9ca15.js"
+  },
+  {
+    "revision": "4ee157fe0671360da7de83afaeb32b07",
+    "url": "/vuetify-swipeout/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vuetify-swipeout/robots.txt"
+  }
+];
